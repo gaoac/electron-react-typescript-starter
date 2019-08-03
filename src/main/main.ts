7e37e176460f3876e2ce91b492ea2c64be4a666d
gaoac <gaoancun@qq.com>
2019-08-03 13:52:23 +0800

    🚨 Lint: 修复eslint报错

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,10 +1,14 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable no-console */
 import { app, BrowserWindow } from 'electron';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import installExtension, {
   REACT_DEVELOPER_TOOLS,
   REDUX_DEVTOOLS,
 } from 'electron-devtools-installer';
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
+// eslint-disable-next-line global-require
 if (require('electron-squirrel-startup')) {
   // eslint-disable-line global-require
   app.quit();
@@ -14,7 +18,7 @@ if (require('electron-squirrel-startup')) {
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow: null | BrowserWindow;
 
-const createWindow = () => {
+const createWindow = (): void => {
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 800,
@@ -23,13 +27,14 @@ const createWindow = () => {
 
   // and load the index.html of the app.
   // @ts-ignore
+  // eslint-disable-next-line no-undef
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
+  mainWindow.on('closed', (): void => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
@@ -37,8 +42,7 @@ const createWindow = () => {
   });
 };
 
-const installdevTools = () => {
-  debugger;
+const installdevTools = (): void => {
   installExtension([REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS])
     .then(name => console.log(`Added Extension:  ${name}`))
     .catch(err => console.log('An error occurred: ', err));
@@ -47,13 +51,13 @@ const installdevTools = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', () => {
+app.on('ready', (): void => {
   createWindow();
   installdevTools();
 });
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
+app.on('window-all-closed', (): void => {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
@@ -61,7 +65,7 @@ app.on('window-all-closed', () => {
   }
 });
 
-app.on('activate', () => {
+app.on('activate', (): void => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
